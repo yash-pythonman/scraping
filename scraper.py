dbe74017ff5c3683b135f7eef6e77eb301774af3
Yashpal Yadav <yashpalyadav1991@gmail.com>
2020-01-06 21:24:27 +0530

    Code for submit form

--- scraper.py
+++ scraper.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
+
 url = "https://data.gov.in/catalog/district-wise-and-month-wise-queries-farmers-kisan-call-centre-kcc-during-2009"
-driver=webdriver.Chrome("drivers/chromedriver/chromedriver")
+driver = webdriver.Chrome("drivers/chromedriver/chromedriver")
 print("driver loaded..............")
 driver.get(url)
 print("url opened ...........")
 element = driver.find_element_by_xpath('//*[@title="csv"]').click()
 print("we got element.........")
-element.click()
+driver.find_element_by_id("edit-name-d").send_keys("test")
+driver.find_element_by_id("edit-mail-d").send_keys("testsel000@gmail.com")
+driver.find_element_by_xpath('//*[@id="edit-submit"]').click()
+driver.find_element_by_id("edit-download-reasons-1").click()
+driver.find_element_by_id("edit-reasons-d-3").click()
 print("element clicked.........")
 driver.quit()
